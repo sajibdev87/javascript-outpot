c930e98e99b7ffd768c0f0c9038bd080fc22ad71
sajibdev87 <145119503+sajibdev87@users.noreply.github.com>
2023-10-18 22:19:20 -0700

    y

---
+++ sajib.js
@@ -0,0 +1,69 @@
+var number = 33;
+console.log(number)
+var todayMona = false;
+console.log(todayMona)
+
+var sajibFanny = false;
+var mamaGood = true;
+var ma ="5685"
+console.log(sajibFanny,mamaGood,ma)
+
+var name = "sajib";
+var last= "bhuiyan";
+var total = name + last;
+console.log(total);
+
+var mola = 56;
+var lolipop = 5;
+var total = mola+lolipop;
+console.log(total); 
+
+var PIE = 3.1416;
+console.log(PIE);
+
+var roti = 65;
+var baji = 56;
+var totalPrice = roti+baji;
+console.log(totalPrice);
+
+var chicken = 3.666777;
+var convert = parseFloat(chicken);
+var convert = parseInt(chicken);
+var cow = 45;
+var superMarkting = chicken + cow;
+console.log(convert);
+
+var amount = 500;
+var price = 45;
+var add = amount + price;
+var division = amount / price;
+var minus = amount - price;
+var multi = amount * price;
+console.log(mart);
+
+var myName = "bhuiyan sajib";
+myName = "sajib bhuiyan";
+console.log(myName);
+
+var price = 56;
+price = 58;
+price = 56+2;
+price += 2;
+price = 56+10;
+price += 10;
+console.log(price);
+
+var myAge = 12;
+myAge --;
+myAge ++;
+console.log(typeof(myAge));
+
+var today = new Date();
+console.log(today);
+
+var cost = 0.4;
+var lost = 0.3;
+var total = cost + lost;
+
+var solve = parseFloat(total.toFixed(3));
+console.log(solve);
